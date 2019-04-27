7c3c32f774961b6c1a93002174984dc3f7aa9507
ugaldedr <Dario.ugalde@mavs.uta.edu>
2019-04-27 06:55:31 -0500

    Added startup.ccs.c to final project

--- tm4c123gh6pm_startup_ccs.c
+++ tm4c123gh6pm_startup_ccs.c
@@ -65,6 +65,7 @@ extern uint32_t __STACK_TOP;
 //*****************************************************************************
 
 extern void timer1ISR(void);               // Refer to TIMER1 handler in main.c
+extern void timer2ISR(void);
 extern void Uart1Isr(void);
 
 #pragma DATA_SECTION(g_pfnVectors, ".intvecs")
@@ -110,7 +111,7 @@ void (* const g_pfnVectors[])(void) =
     IntDefaultHandler,                      // Timer 0 subtimer B
     timer1ISR,                              // Timer 1 subtimer A
     IntDefaultHandler,                      // Timer 1 subtimer B
-    IntDefaultHandler,                      // Timer 2 subtimer A
+    timer2ISR,                              // Timer 2 subtimer A
     IntDefaultHandler,                      // Timer 2 subtimer B
     IntDefaultHandler,                      // Analog Comparator 0
     IntDefaultHandler,                      // Analog Comparator 1
